9576111dbac0c211040810a564650477e3644eff
swsgit <sws@tuta.io>
2018-04-24 13:27:44 -0700

    fix output preview, add section

--- lab5.cpp
+++ lab5.cpp
@@ -1,4 +1,5 @@
 // Spencer Stone
+// Section 15245 T/Th
 // Lab #5A
 #include <iostream>
 #include <fstream>
@@ -18,13 +19,13 @@ int main() {
 
 	outfile << showpoint << fixed << setprecision(2);
 
-	infile.open("C:\\Users\\student\\Desktop\\data.txt");
+	infile.open("C:\\Users\\Stone\\Desktop\\data.txt");
 	if (!infile)
 	{
 		cout << "Cannot open file, terminating program" << endl;
 		exit(1);
 	}
-	outfile.open("C:\\Users\\student\\Desktop\\result.txt");
+	outfile.open("C:\\Users\\Stone\\Desktop\\result.txt");
 	while (!infile.eof()) {
 		infile >> n;
 		min = n;
@@ -67,5 +68,47 @@ min: 90
 max: 982
 total: 2916
 average: 416.57
+------------------------------------------
+Input: 415 948 126 4 558 571 87
+min: 4
+max: 948
+total: 2709
+average: 387.00
+------------------------------------------
+Input: 42 360 412 721 463 47 119
+min: 42
+max: 721
+total: 2164
+average: 309.14
+------------------------------------------
+Input: 441 190 985 214 509 2 571
+min: 2
+max: 985
+total: 2912
+average: 416.00
+------------------------------------------
+Input: 77 81 681 651 995 93 74
+min: 74
+max: 995
+total: 2652
+average: 378.86
+------------------------------------------
+Input: 310 9 995 561 92 14 288
+min: 9
+max: 995
+total: 2269
+average: 324.14
+------------------------------------------
+Input: 466 664 892 8 766 34 639
+min: 8
+max: 892
+total: 3469
+average: 495.57
+------------------------------------------
+Input: 151 64 98 813 67 834 369
+min: 64
+max: 834
+total: 2396
+average: 342.29
 ------------------------------------------
 **/
