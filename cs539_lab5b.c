cd5a460a5da64aacff75428789bbf438f8f1be21
swsgit <sws@tuta.io>
2018-10-16 20:32:43 -0700

    Add files via upload

---
+++ cs539_lab5b.c
@@ -0,0 +1,203 @@
+/*
+        Stone, Spencer
+        11/10/18
+        Lab 5B
+        Description: Replace the contents of a string 
+                     with the contents of another string.
+        Compile: gcc main.c -O2 -Wall -Werror -std=gnu11 -o main
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define VALID_INPUT(x) ((x >= 'A' && x <= 'Z') || x == '\n' || x == '\0')
+
+char *generate_s1(void);
+char *generate_s2(char *c);
+char *strfilter(char *s1, char *s2, char c);
+int _strlen(char *str);
+
+int main(void) {
+  char c;
+  char *s1 = generate_s1();
+
+  for (;;) {
+    char *s2 = generate_s2(&c);
+    char *fs1 = strfilter(s1, s2, c);
+
+    printf("s1          = {\"%s\"}\n", s1);
+    printf("s2          = {\"%s\"}\n", s2);
+    printf("c           = {\"%c\"}\n", c);
+    printf("filtered s1 = {\"%s\"}\n", fs1);
+  }
+
+  return 1;
+}
+
+char *generate_s1(void) {
+  static char s1[41];
+  static char *ptr = &s1[0];
+  /* seed rand() so it produces unique results each run
+     cast to unsigned int to avoid compiler warning */
+  srand((unsigned int)time(NULL));
+  for (int i = 0; i < 40; i++, (void)*(ptr++)) {
+    /* return random number between [M, N]
+       M + rand() / (RAND_MAX / (N - M + 1) + 1) */
+    *ptr = (char)(65 + rand() / (RAND_MAX / (90 - 65 + 1) + 1));
+  }
+  /* finalize the string a with null terminator */
+  *ptr = '\0';
+
+  return s1;
+}
+
+char *generate_s2(char *c) {
+  static char s2[23];
+  static char *ptr = &s2[0];
+  bool done = false;
+  int len;
+  int i;
+  int ch;
+  int old_ch = '\n';
+  int count = 0;
+
+  while (!done) {
+    /* get s2 from user then clear stdin */
+    printf("Enter uppercase characters [A, Z]: ");
+    char *ret = fgets(s2, sizeof s2, stdin);
+    if (ret == NULL) {
+      exit(EXIT_FAILURE);
+    }
+    fseek(stdin, 0, SEEK_END);
+
+    /* find newline character and replace it with null terminator */
+    for (i = 0; i < sizeof s2; i++, (void)*(ptr++)) {
+      if (*ptr == '\n') {
+        *ptr = '\0';
+      }
+    }
+    /* shift ptr back to starting position */
+    for (i = 0; i < sizeof s2; i++, (void)*(ptr--));
+    /* check if string meets requirements:
+       minimum 2 chars, max 20 chars, [A-Z] */
+    len = _strlen(s2);
+    i = 0;
+    while (!done) {
+      if (len < 2 || len > 20) {
+        printf("String length must be bewtween [2, 20]\n");
+        break;
+      } else if (!VALID_INPUT(*ptr)) {
+        printf("String must contain uppercase letters [A-Z]\n");
+        break;
+      } else if (i == len) {
+        done = true;
+        break;
+      }
+      (void)*(ptr++);
+      i++;
+    }
+  }
+  /* get replacement character from stdin,
+     replacement char must be a single char */
+  done = false;
+  printf("Enter replacement character: ");
+  while (!done && (ch = getchar())) {
+    if (count > 1) {
+      fseek(stdin, 0, SEEK_END);
+      printf("Replacement character must be a single character\n");
+      printf("Enter replacement character: ");
+      count = 0;
+      old_ch = 0;
+      continue;
+    } else if (ch == '\n' && old_ch != '\n') {
+      *c = old_ch;
+      done = true;
+    }
+    count++;
+    old_ch = ch;
+  }
+  fseek(stdin, 0, SEEK_END);
+
+  return s2;
+}
+
+char *strfilter(char *s1, char *s2, char c) {
+  static char fs1[41];
+  static char *fs1_ptr = &fs1[0];
+  char *s1_ptr = &s1[0];
+  char *s2_ptr = &s2[0];
+  int s1_len = _strlen(s1);
+  int s2_len = _strlen(s2);
+  int fs1_len = s1_len;
+
+  /* copy contents of s1_ptr into fs1_ptr */
+  for (int i = 0; i < s1_len; i++) {
+    *fs1_ptr = *s1_ptr;
+    (void)*(s1_ptr++); 
+    (void)*(fs1_ptr++);
+  }
+  /* shift fs1_ptr and s1_ptr to start */
+  for (int i = 0; i < s1_len; i++, (void)*(s1_ptr--), (void)*(fs1_ptr--));
+
+  /* find occurences of s2_ptr in fs1_ptr and replace */
+  for (int i = 0; i < s2_len; i++) {
+    for (int j = 0; j < fs1_len; j++) {
+      if (*fs1_ptr == *s2_ptr) {
+        *fs1_ptr = c;
+      }
+      (void)*(fs1_ptr++);
+    }
+    /* rewind fs1_ptr after every full loop of its contents */
+    for (int j = 0; j < fs1_len; j++, (void)*(fs1_ptr--));
+    (void)*(s2_ptr++);
+  }
+
+  /* shift s2_ptr to start */
+  for (int i = 0; i < s2_len; i++, (void)*(s2_ptr--));
+
+  return fs1;
+}
+
+int _strlen(char *str) {
+  char *ptr = &str[0];
+  int i = 0;
+  while (*ptr != '\0') {
+    i++;
+    (void)*(ptr++);
+  }
+  return i;
+}
+/*
+Enter uppercase characters [A, Z]:
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: test
+String must contain uppercase letters [A-Z]
+Enter uppercase characters [A, Z]: A
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: AAAAAAAAAAAAAAAAAAAAAAAAA
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: AL
+Enter replacement character: *
+s1          = {"OGXEKOMNDYZCILLTADZPMLRFTHJSKRRNRDLFLCFR"}
+s2          = {"AL"}
+c           = {"*"}
+filtered s1 = {"OGXEKOMNDYZCI**T*DZPM*RFTHJSKRRNRD*F*CFR"}
+Enter uppercase characters [A, Z]: TEST
+Enter replacement character: .
+s1          = {"OGXEKOMNDYZCILLTADZPMLRFTHJSKRRNRDLFLCFR"}
+s2          = {"TEST"}
+c           = {"."}
+filtered s1 = {"OGX.KOMNDYZCILL.ADZPMLRF.HJ.KRRNRDLFLCFR"}
+Enter uppercase characters [A, Z]: 
+----------------------------------------------------------------
+Enter uppercase characters [A, Z]: ABC
+Enter replacement character: ./
+Replacement character must be a single character
+Enter replacement character: .
+s1          = {"LSCZMOOQQUOAKXHLJELCODQNFJNMBLVMGNXRSTQK"}
+s2          = {"ABC"}
+c           = {"."}
+filtered s1 = {"LS.ZMOOQQUO.KXHLJEL.ODQNFJNM.LVMGNXRSTQK"}
+Enter uppercase characters [A, Z]:
+*/
