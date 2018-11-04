e7377a0a4922cfb7f1c9f875f52050cdeb2e15b4
swsgit <sws@tuta.io>
2018-11-03 23:15:36 -0700

    restructure

---
+++ cs539/cs539_lab3b.c
@@ -0,0 +1,235 @@
+/*
+	Stone, Spencer
+	Lab 3B
+	10/15/18
+	Description: A calculator that accepts input from user's stdin
+	Compile: gcc main.c -Wall -Werror -Wextra -O2 -std=gnu11 -o main
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define IS_INT(x) (x >= 48 && x <= 57)
+#define IS_ADD(x) (x == '+')
+#define IS_SUB(x) (x == '-')
+#define IS_MUL(x) (x == '*')
+#define IS_DIV(x) (x == '/')
+#define IS_MOD(x) (x == '%')
+#define IS_NWL(x) (x == '\n')
+#define IS_EOF(x) (x == EOF)
+#define IS_OPR(x) (IS_ADD(x) || IS_SUB(x) || IS_MUL(x) || IS_DIV(x) || IS_MOD(x))
+#define IS_VAL(x) (IS_INT(x) || IS_OPR(x))
+
+void reset(int *total, char *op, int *first_number, int *num);
+char get_number(int *num);
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+int mod(int a, int b);
+
+int main(void) {
+	char op = 0;
+	int num = 0;
+	int total = 0;
+	int first_number = 1;
+	printf("calc> ");
+	while (1) {
+		if (IS_EOF(op)) {
+			printf(" = %d\n", total);
+			exit(EXIT_SUCCESS);
+		}
+		else if (IS_NWL(op)) {
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);
+		} 
+		else if (IS_ADD(op)) {
+			op = get_number(&num);
+			printf("%d + %d", total, num);
+			if (!IS_NWL(op)) {
+				printf("\nInvalid syntax...\ncalc> ");
+				reset(&total, &op, &first_number, &num);
+				continue;
+			}
+			total = add(total, num);
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);
+			continue;
+		} 
+		else if (IS_SUB(op)) { 
+			op = get_number(&num); 
+			printf("%d - %d", total, num);
+			if (!IS_NWL(op)) {
+				printf("\nInvalid syntax...\ncalc> ");
+				reset(&total, &op, &first_number, &num);
+				continue;
+			}
+			total = subtract(total, num);
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);
+		} 
+		else if (IS_MUL(op)) { 
+			op = get_number(&num); 
+			total = multiply(total, num);
+			printf("%d * %d", total, num);
+			if (!IS_NWL(op)) {
+				printf("\nInvalid syntax...\ncalc> ");
+				reset(&total, &op, &first_number, &num);
+				continue;
+			}
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);  
+		} 
+		else if (IS_DIV(op)) { 
+			op = get_number(&num);
+			printf("%d / %d", total, num);
+			if (!IS_NWL(op)) {
+				printf("\nInvalid syntax...\ncalc> ");
+				reset(&total, &op, &first_number, &num);
+				continue;
+			} 
+			total = divide(total, num);
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);
+		}
+		else if (IS_MOD(op)) { 
+			op = get_number(&num);
+			printf("%d %% %d", total, num);
+			if (!IS_NWL(op)) {
+				printf("\nInvalid syntax...\ncalc> ");
+				reset(&total, &op, &first_number, &num);
+				continue;
+			}  
+			total = mod(total, num);
+			printf(" = %d\n", total);
+			printf("calc> ");
+			reset(&total, &op, &first_number, &num);
+		} 
+		else {
+			op = get_number(&num);
+			if (first_number) {
+				total = num;
+				first_number = 0;
+			}
+		}
+	}
+	return 1;
+}
+
+char get_number(int *num) {
+	int first_run = 1;
+	char ch;
+	int tmp;
+	*num = 0;
+	while ((ch = getchar()) != EOF) {
+		if (IS_NWL(ch)) { return ch; }
+		if (IS_OPR(ch)) { return ch; }
+		if (!IS_VAL(ch)) { continue; }
+		if (first_run) {
+			tmp = ch - '0';
+			*num = tmp;
+			first_run = 0;
+			continue;
+		}
+		if(!first_run) {
+			tmp = ch - '0';
+			*num = multiply(*num, 10);
+			*num = add(*num, tmp);
+			continue;
+		}
+	}
+	return ch;
+}
+
+void reset(int *total, char *op, int *first_number, int *num) {
+	fseek(stdin,0,SEEK_END);
+	*total = 0;
+	*op = 0;
+	*first_number = 1;
+	*num = 0;
+}
+
+int add(int a, int b) {
+	if ((b > 0) && (a > INT_MAX - b)) {
+		printf("\nError: integer overflow (%d + %d)\n", a, b);
+	} else if ((b < 0) && (a < INT_MIN - b)) {
+		printf("\nError: integer underflow (%d + %d)\n", a, b);
+	} 
+	return a + b;
+}
+int subtract(int a, int b) {
+	if ((b < 0) && (a > INT_MAX + b)) {
+		printf("\nError: integer overflow (%d - %d)\n", a, b);
+	} else if ((b > 0) && (a < INT_MIN + b)) {
+		printf("\nError: integer underflow (%d - %d)\n", a, b);
+	} 
+	return a - b;
+}
+int multiply(int a, int b) {
+	if (a > INT_MAX / b) {
+		printf("\nError: integer overflow (%d * %d)\n", a, b);
+	} else if (a < INT_MIN / b) {
+		printf("\nError: integer underflow (%d * %d)\n", a, b);
+	} else if ((a == -1) && (b == INT_MIN)) {
+		printf("\nError: possible integer overflow (%d * %d)\n", a, b);
+	} else if ((b == -1) && (a == INT_MIN)) {
+		printf("\nError: possible integer overflow (%d * %d)\n", a, b);
+	} 
+	return a * b;
+}
+int divide(int a, int b) { 
+	if (b == 0) {
+		printf("\nError: cannot divide by zero (%d / %d)", a, b);
+		return 0;
+	} else if ((a == -1) && (b == INT_MIN)) {
+		printf("\nError: possible integer overflow (%d / %d)\n", a, b);
+	} else if ((b == -1) && (a == INT_MIN)) {
+		printf("\nError: possible integer overflow (%d / %d)\n", a, b);
+	} 
+	return a / b;
+}
+int mod(int a, int b) { 
+	if (b == 0) {
+		printf("\nError: cannot do modulo division by zero (%d %% %d)", a, b);
+		return 0;
+	}  
+	return a % b;
+}
+/*
+# ./main
+calc> 100 - 1
+100 - 1 = 99
+calc> 100 + 1
+100 + 1 = 101
+calc> 100 * 10
+1000 * 10 = 1000
+calc> 1000 / 10
+1000 / 10 = 100
+calc> 1001 % 10
+1001 % 10 = 1
+calc> 10 / 0
+10 / 0
+Error: cannot divide by zero (10 / 0) = 0
+calc> 10 % 0
+10 % 0
+Error: cannot do modulo division by zero (10 % 0)= 0
+calc> 10000000 * 100000000
+Error: integer overflow (10000000 * 100000000)
+-1530494976 * 100000000 = -1530494976
+calc> 10 + 10 + 10
+10 + 10
+Invalid syntax...
+calc> 10 - 10 - 10
+10 - 10
+Invalid syntax...
+calc> -10
+0 - 10 = -10
+calc> +10
+0 + 10 = 10
+calc>
+*/
---
+++ cs539/cs539_lab4b.c
@@ -0,0 +1,182 @@
+/*
+        Stone, Spencer
+        11/10/18
+        Lab 4B
+        Description: Replace the contents of a string
+                     with the contents of another string.
+                     Using a replacement character.
+        Compile: gcc main.c -O2 -Wall -Werror -std=gnu11 -o main
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define VALID_INPUT(x) ((x >= 'A' && x <= 'Z') || x == '\n' || x == '\0')
+
+char *generate_s1(void);
+char *generate_s2(char *c);
+char *strfilter(char s1[], char s2[], char c);
+int _strlen(char str[]);
+
+int main(void) {
+  char c;
+  char *s1 = generate_s1();
+
+  for (;;) {
+    char *s2 = generate_s2(&c);
+    char *fs1 = strfilter(s1, s2, c);
+
+    printf("s1          = {\"%s\"}\n", s1);
+    printf("s2          = {\"%s\"}\n", s2);
+    printf("c           = {\"%c\"}\n", c);
+    printf("filtered s1 = {\"%s\"}\n", fs1);
+  }
+
+  return 1;
+}
+
+char *generate_s1(void) {
+  static char s1[41];
+  /* seed rand() so it produces unique results each run
+     cast to unsigned int to avoid compiler warning */
+  srand((unsigned int)time(NULL));
+  for (int i = 0; i < 40; i++) {
+    /* return random number between [M, N]
+       M + rand() / (RAND_MAX / (N - M + 1) + 1) */
+    s1[i] = 65 + rand() / (RAND_MAX / (90 - 65 + 1) + 1);
+  }
+  /* finalize the string a with null terminator */
+  s1[40] = '\0';
+
+  return s1;
+}
+
+char *generate_s2(char *c) {
+  static char s2[23];
+  bool done = false;
+  int len;
+  int i;
+  int ch;
+  int count = 0;
+  int old_ch = 0;
+
+  while (!done) {
+    /* get s2 from user then clear stdin */
+    printf("Enter uppercase characters [A, Z]: ");
+    char *ret = fgets(s2, sizeof s2, stdin);
+    if (ret == NULL) {
+      exit(EXIT_FAILURE);
+    }
+    fseek(stdin, 0, SEEK_END);
+    /* find newline character and replace it with null terminator */
+    for (i = 0; i < sizeof s2; i++) {
+      if (s2[i] == '\n') {
+        s2[i] = '\0';
+      }
+    }
+    /* check if string meets requirements:
+       minimum 2 chars, max 20 chars, [A-Z] */
+    len = _strlen(s2);
+    if (len < 2 || len > 20) {
+      printf("String length must be bewtween [2, 20]\n");
+      continue;
+    }
+    i = 0;
+    while (!done) {
+      if (!VALID_INPUT(s2[i])) {
+        printf("String must contain uppercase letters [A-Z]\n");
+        break;
+      } else if (i == len) {
+        done = true;
+        break;
+      }
+      i++;
+    }
+  }
+
+  /* get replacement character from stdin,
+       replacement char must be a single char */
+  done = false;
+  printf("Enter replacement character: ");
+  while (!done && (ch = getchar())) {
+    if (count > 1) {
+      fseek(stdin, 0, SEEK_END);
+      printf("Replacement character must be a single character\n");
+      printf("Enter replacement character: ");
+      count = 0;
+      old_ch = 0;
+      continue;
+    } else if (ch == '\n' && old_ch != '\n') {
+      *c = old_ch;
+      done = true;
+    }
+    count++;
+    old_ch = ch;
+  }
+  fseek(stdin, 0, SEEK_END);
+  return s2;
+}
+
+char *strfilter(char s1[], char s2[], char c) {
+  static char fs1[41];
+  int s1_len = _strlen(s1);
+  int s2_len = _strlen(s2);
+  int fs1_len;
+
+  /* copy contents of s1 into str */
+  for (int i = 0; i < s1_len; i++) {
+    fs1[i] = s1[i];
+  }
+
+  fs1_len = _strlen(fs1);
+  for (int i = 0; i < s2_len; i++) {
+    for (int j = 0; j < fs1_len; j++) {
+      if (fs1[j] == s2[i]) {
+        fs1[j] = c;
+      }
+    }
+  }
+
+  return fs1;
+}
+
+int _strlen(char str[]) {
+  int i = 0;
+  while (str[i] != '\0')
+    i++;
+  return i;
+}
+/*
+Enter uppercase characters [A, Z]:
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: test
+String must contain uppercase letters [A-Z]
+Enter uppercase characters [A, Z]: A
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: AAAAAAAAAAAAAAAAAAAAAAAAA
+String length must be bewtween [2, 20]
+Enter uppercase characters [A, Z]: AL
+Enter replacement character: *
+s1          = {"OGXEKOMNDYZCILLTADZPMLRFTHJSKRRNRDLFLCFR"}
+s2          = {"AL"}
+c           = {"*"}
+filtered s1 = {"OGXEKOMNDYZCI**T*DZPM*RFTHJSKRRNRD*F*CFR"}
+Enter uppercase characters [A, Z]: TEST
+Enter replacement character: .
+s1          = {"OGXEKOMNDYZCILLTADZPMLRFTHJSKRRNRDLFLCFR"}
+s2          = {"TEST"}
+c           = {"."}
+filtered s1 = {"OGX.KOMNDYZCILL.ADZPMLRF.HJ.KRRNRDLFLCFR"}
+Enter uppercase characters [A, Z]:
+----------------------------------------------------------------
+Enter uppercase characters [A, Z]: ABC
+Enter replacement character: ./
+Replacement character must be a single character
+Enter replacement character: .
+s1          = {"LSCZMOOQQUOAKXHLJELCODQNFJNMBLVMGNXRSTQK"}
+s2          = {"ABC"}
+c           = {"."}
+filtered s1 = {"LS.ZMOOQQUO.KXHLJEL.ODQNFJNM.LVMGNXRSTQK"}
+Enter uppercase characters [A, Z]:
+*/
