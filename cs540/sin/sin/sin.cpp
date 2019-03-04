56b3d63b14e097983c3a4809a1dff4dbc4b5c909
swsgit <sws@tuta.io>
2019-03-03 23:47:33 -0800

    add both hw

---
+++ cs540/sin/sin/sin.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double factorial(int n) {
+  double tmp = 1.00;
+  for (int i = n; i > 0; i--) {
+    tmp *= i;
+  }
+  return tmp;
+}
+
+double _sin(double x) {
+  return x - (pow(x, 3) / factorial(3)) + (pow(x, 5) / factorial(5)) -
+         (pow(x, 7) / factorial(7)) + (pow(x, 9) / factorial(9)) -
+         (pow(x, 11) / factorial(11)) + (pow(x, 13) / factorial(13));
+}
+
+int main() {
+  constexpr auto PI = 3.14159265358979323846;
+  printf("%f\n", _sin(PI));
+
+  system("pause");
+  return 0;
+}
---
+++ cs540/stack/stack/stack.cpp
@@ -0,0 +1,105 @@
+// Spencer Stone
+#include <iostream>
+
+using namespace std;
+
+typedef struct Stack {
+  int item;
+  Stack *next;
+} Stack;
+
+void Push(Stack **s, int item) {
+  Stack *currentStack = *s;
+  Stack *newStack = new Stack;
+  newStack->item = item;
+  newStack->next = NULL;
+
+  if (currentStack == NULL) {
+    *s = newStack;
+    return;
+  }
+
+  while (currentStack != NULL) {
+    if (currentStack->next == NULL) {
+      currentStack->next = newStack;
+      break;
+    }
+    currentStack = currentStack->next;
+  }
+}
+
+int Pop(Stack **s) {
+  int ret = NULL;
+  Stack *currentStack = *s;
+  Stack *prevStack = NULL;
+
+  while (currentStack != NULL) {
+    if (currentStack->next == NULL) {
+      ret = currentStack->item;
+      delete[] currentStack;
+      if (prevStack != NULL) {
+        prevStack->next = NULL;
+      } else {
+        *s = NULL;
+      }
+      return ret;
+    }
+    prevStack = currentStack;
+    currentStack = currentStack->next;
+  }
+
+  return ret;
+}
+
+void PrintStack(Stack *s) {
+  while (s != NULL) {
+    cout << s->item << endl;
+    if (s->next == NULL) {
+      break;
+    }
+    s = s->next;
+  }
+}
+
+void DeleteStack(Stack **s) {
+  Stack *currentStack = *s;
+  Stack *nextStack;
+  while (currentStack != NULL) {
+    nextStack = currentStack->next;
+    delete[] currentStack;
+    currentStack = nextStack;
+  }
+  *s = NULL;
+}
+
+int main() {
+  Stack *head = NULL;
+
+  Push(&head, 10);
+  Push(&head, 20);
+  Push(&head, 30);
+
+  cout << "Printing stack...\n";
+  PrintStack(head);
+
+  cout << "Popping stack: " << Pop(&head) << endl;
+  cout << "Popping stack: " << Pop(&head) << endl;
+  cout << "Popping stack: " << Pop(&head) << endl;
+
+  Push(&head, 40);
+  Push(&head, 50);
+  Push(&head, 60);
+
+  cout << "Printing stack...\n";
+  PrintStack(head);
+
+  cout << "Deleting stack...\n";
+  DeleteStack(&head);
+
+  cout << "Printing stack...\n";
+  PrintStack(head);
+
+  system("pause");
+
+  return 0;
+}
